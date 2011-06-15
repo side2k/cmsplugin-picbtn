da102c3b8b19ace728ef78bdcb87996aa9bc3181
outsider <siderelay@gmail.com>
2011-06-15 10:48:39 +0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cmsplugin_picbtn',
+      version='0.1',
+      description='PictureButton plugin for django-cms',
+      author='Leonid Amirov',
+      author_email='amirov@atvc.ru',
+      url='http://www.atknet.ru/',
+      packages=['cmsplugin_picbtn'],
+      )
